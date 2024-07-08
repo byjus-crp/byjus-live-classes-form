938c0c57d490c9355a86e7af95ec6854d628bec5
byjus-crp <info@byjuscrp.com>
2024-07-08 19:57:22 +0530

    initial

---
+++ app.js
@@ -0,0 +1,311 @@
+const timeslots = {
+    "boards": {
+        "cbse": {
+            "states": {
+                "andhra-pradesh": {
+                    "grades": {
+                        "4": {
+                            "subjects": {
+                                "mathSci": {
+                                    "batches": {
+                                        "A1": "9:00 AM - 10:00 AM",
+                                        "A2": "10:00 AM - 11:00 AM"
+                                    }
+                                },
+                                "english": {
+                                    "batches": {
+                                        "B1": "11:00 AM - 12:00 PM",
+                                        "B2": "12:00 PM - 1:00 PM"
+                                    }
+                                },
+                                "sst": {
+                                    "batches": {
+                                        "C1": "1:00 PM - 2:00 PM",
+                                        "C2": "2:00 PM - 3:00 PM"
+                                    }
+                                }
+                            }
+                        },
+                        "5": {
+                            "subjects": {
+                                "mathSci": {
+                                    "batches": {
+                                        "A1": "9:00 AM - 10:00 AM",
+                                        "A2": "10:00 AM - 11:00 AM"
+                                    }
+                                },
+                                "english": {
+                                    "batches": {
+                                        "B1": "11:00 AM - 12:00 PM",
+                                        "B2": "12:00 PM - 1:00 PM"
+                                    }
+                                },
+                                "sst": {
+                                    "batches": {
+                                        "C1": "1:00 PM - 2:00 PM",
+                                        "C2": "2:00 PM - 3:00 PM"
+                                    }
+                                }
+                            }
+                        }
+                        // Additional grades 6 to 10 can be added similarly
+                    }
+                },
+                "telangana": {
+                    "grades": {
+                        "4": {
+                            "subjects": {
+                                "mathSci": {
+                                    "batches": {
+                                        "A1": "9:00 AM - 10:00 AM",
+                                        "A2": "10:00 AM - 11:00 AM"
+                                    }
+                                },
+                                "english": {
+                                    "batches": {
+                                        "B1": "11:00 AM - 12:00 PM",
+                                        "B2": "12:00 PM - 1:00 PM"
+                                    }
+                                },
+                                "sst": {
+                                    "batches": {
+                                        "C1": "1:00 PM - 2:00 PM",
+                                        "C2": "2:00 PM - 3:00 PM"
+                                    }
+                                }
+                            }
+                        },
+                        "5": {
+                            "subjects": {
+                                "mathSci": {
+                                    "batches": {
+                                        "A1": "9:00 AM - 10:00 AM",
+                                        "A2": "10:00 AM - 11:00 AM"
+                                    }
+                                },
+                                "english": {
+                                    "batches": {
+                                        "B1": "11:00 AM - 12:00 PM",
+                                        "B2": "12:00 PM - 1:00 PM"
+                                    }
+                                },
+                                "sst": {
+                                    "batches": {
+                                        "C1": "1:00 PM - 2:00 PM",
+                                        "C2": "2:00 PM - 3:00 PM"
+                                    }
+                                }
+                            }
+                        }
+                        // Additional grades 6 to 10 can be added similarly
+                    }
+                }
+                // Additional states can be added similarly
+            }
+        },
+        "icse": {
+            "states": {
+                "andhra-pradesh": {
+                    "grades": {
+                        "4": {
+                            "subjects": {
+                                "mathSci": {
+                                    "batches": {
+                                        "A1": "9:00 AM - 10:00 AM",
+                                        "A2": "10:00 AM - 11:00 AM"
+                                    }
+                                },
+                                "english": {
+                                    "batches": {
+                                        "B1": "11:00 AM - 12:00 PM",
+                                        "B2": "12:00 PM - 1:00 PM"
+                                    }
+                                },
+                                "sst": {
+                                    "batches": {
+                                        "C1": "1:00 PM - 2:00 PM",
+                                        "C2": "2:00 PM - 3:00 PM"
+                                    }
+                                }
+                            }
+                        },
+                        "5": {
+                            "subjects": {
+                                "mathSci": {
+                                    "batches": {
+                                        "A1": "9:00 AM - 10:00 AM",
+                                        "A2": "10:00 AM - 11:00 AM"
+                                    }
+                                },
+                                "english": {
+                                    "batches": {
+                                        "B1": "11:00 AM - 12:00 PM",
+                                        "B2": "12:00 PM - 1:00 PM"
+                                    }
+                                },
+                                "sst": {
+                                    "batches": {
+                                        "C1": "1:00 PM - 2:00 PM",
+                                        "C2": "2:00 PM - 3:00 PM"
+                                    }
+                                }
+                            }
+                        }
+                        // Additional grades 6 to 10 can be added similarly
+                    }
+                },
+                "telangana": {
+                    "grades": {
+                        "4": {
+                            "subjects": {
+                                "mathSci": {
+                                    "batches": {
+                                        "A1": "9:00 AM - 10:00 AM",
+                                        "A2": "10:00 AM - 11:00 AM"
+                                    }
+                                },
+                                "english": {
+                                    "batches": {
+                                        "B1": "11:00 AM - 12:00 PM",
+                                        "B2": "12:00 PM - 1:00 PM"
+                                    }
+                                },
+                                "sst": {
+                                    "batches": {
+                                        "C1": "1:00 PM - 2:00 PM",
+                                        "C2": "2:00 PM - 3:00 PM"
+                                    }
+                                }
+                            }
+                        },
+                        "5": {
+                            "subjects": {
+                                "mathSci": {
+                                    "batches": {
+                                        "A1": "9:00 AM - 10:00 AM",
+                                        "A2": "10:00 AM - 11:00 AM"
+                                    }
+                                },
+                                "english": {
+                                    "batches": {
+                                        "B1": "11:00 AM - 12:00 PM",
+                                        "B2": "12:00 PM - 1:00 PM"
+                                    }
+                                },
+                                "sst": {
+                                    "batches": {
+                                        "C1": "1:00 PM - 2:00 PM",
+                                        "C2": "2:00 PM - 3:00 PM"
+                                    }
+                                }
+                            }
+                        }
+                        // Additional grades 6 to 10 can be added similarly
+                    }
+                }
+                // Additional states can be added similarly
+            }
+        }
+        // Additional boards can be added similarly
+    }
+};
+
+// CODE STARTS HERE
+
+    const stateSelect = document.getElementById('state');
+    const boardSelect = document.getElementById('board');
+    const grade = "4"; //Replace with shopify grade variable
+
+    stateSelect.addEventListener('change', checkSelection);
+    boardSelect.addEventListener('change', checkSelection);
+
+
+function checkSelection() {
+    const stateValue = stateSelect.value;
+    const boardValue = boardSelect.value;
+
+    if (stateValue && boardValue) {
+        // Trigger your function here
+        console.log("Both state and board are selected:", stateValue, boardValue);
+        populateSlots();
+    }
+}
+
+
+function getBatchesAndTimeslots(board, state, grade) {
+    console.log(state, board, grade);
+    if (
+        timeslots.boards[board] &&
+        timeslots.boards[board].states[state] &&
+        timeslots.boards[board].states[state].grades[grade]
+    ) {
+        const subjects = timeslots.boards[board].states[state].grades[grade].subjects;
+        return subjects;
+    } else {
+        return null; // Return null if any of the specified keys are not found
+    }
+}
+
+
+function populateSlots(){
+    const board = getSelectedBoard();
+    const state = getSelectedState();
+    const subjects = getBatchesAndTimeslots(board, state, grade);
+    populateBatches(subjects);
+
+}
+
+function getSelectedBoard(){
+    return boardSelect.value;
+}
+
+function getSelectedState(){
+    return stateSelect.value;
+}
+
+function populateBatches(subjects) {
+    Object.keys(subjects).forEach(subjectKey => {
+        const subject = subjects[subjectKey];
+        const batches = subject.batches;
+
+        const accordionContentId = `${subjectKey}AccordianContent`;
+        const accordionContent = document.getElementById(accordionContentId);
+
+        // Clear existing content
+        accordionContent.innerHTML = '';
+
+        if (subjectKey === "mathSci") {
+            // Create radio buttons for mathSci
+            Object.keys(batches).forEach(batchKey => {
+                const radioDiv = document.createElement('div');
+                
+                const radioButton = document.createElement('input');
+                radioButton.setAttribute('type', 'radio');
+                radioButton.setAttribute('name', 'mathSciBatch');
+                radioButton.setAttribute('value', batches[batchKey]);
+                radioButton.setAttribute('id', `${batchKey}`);
+
+                const label = document.createElement('label');
+                label.setAttribute('for', `${batchKey}`);
+                label.textContent = `${batchKey} (${batches[batchKey]})`;
+
+                radioDiv.appendChild(radioButton);
+                radioDiv.appendChild(label);
+                accordionContent.appendChild(radioDiv);
+            });
+        } else {
+            // Create a select element for other subjects
+            const selectElement = document.createElement('select');
+            selectElement.setAttribute('id', `${subjectKey}Select`);
+
+            Object.keys(batches).forEach(batchKey => {
+                const option = document.createElement('option');
+                option.setAttribute('value', batches[batchKey]);
+                option.textContent = `${batchKey} (${batches[batchKey]})`;
+                selectElement.appendChild(option);
+            });
+
+            accordionContent.appendChild(selectElement);
+        }
+    });
+}
